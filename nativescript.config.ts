63148a62c3f9a7a0c115e472e6d32c435b7035ec
tomasmcm <tomasmcm@whitesmith.co>
2022-07-03 13:43:48 +0100

    update ids

--- nativescript.config.ts
+++ nativescript.config.ts
@@ -1,7 +1,7 @@
 import { NativeScriptConfig } from '@nativescript/core';
 
 export default {
-  id: 'org.nativescript.zazen',
+  id: 'co.whitesmith.zazentimer',
   appPath: 'app',
   appResourcesPath: 'App_Resources',
   android: {
